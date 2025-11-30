a8de940e35554cf7525919a13af25c5f09e666f8
aymanebenomar <aymanebenomar2005@gmail.com>
2025-11-30 16:56:09 +0100

    initialising the prjct

---
+++ careerrafrontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ careerrafrontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",           // root HTML
+    "./src/**/*.{js,jsx,ts,tsx}" // all JS/TS/JSX/TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
