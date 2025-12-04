c5a72c681af9eb515332bb2e1baf438cf871793e
aymanebenomar <aymanebenomar2005@gmail.com>
2025-12-04 18:55:37 +0100

    Add section navigation and progress bar to ResumeBuilder component

--- careerrafrontend/src/pages/ResumeBuilder.jsx
+++ careerrafrontend/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 import Dashboard from './Dashboard';
 
 const ResumeBuilder = () => {
@@ -61,8 +61,35 @@ const ResumeBuilder = () => {
 		<div className='max-w-7xl mx-auto px-4 pb-8'>
 			<div className='grid lg:grid-cols-12 gap-8'>
 				{/* Left Panel - Form */}
-				<div>
+				<div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+					<div className='bg-white rounded-lg shadow-sm border border-grey-200 p-6 pt-1'>
+						{/* Progress barre - using activeSectionIndex */}
+						<hr  className='absolute top-0 left-0 right-0 border-2 border-gray-200'/>
+						<hr className='absolute top-0 left-0 h-1 bg-gradient-to-r
+						from-green-500 to-green-700 border-none transition-all duration-2000' 
+						style={{width: `${activeSectionIndex * 100 / (sections.length - 1)}%`}} />
 
+						{/* Section Navigation */}
+						<div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+							<div></div>
+							<div className='flex items-center'>
+								{activeSectionIndex !== 0 && (
+									<button onClick={() => setActiveSectionIndex((prevIndex) => Math.max(prevIndex - 1, 0))} 
+									className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600
+									hover:bg-gray-50 transition-all' disabled={activeSectionIndex === 0}>
+										<ChevronLeft className='size-4' /> Previous
+									</button>
+								)}
+								<button onClick={() => setActiveSectionIndex((prevIndex) => Math.min(prevIndex + 1, sections.length - 1))} 
+									className={`flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50
+									transition-all ${activeSectionIndex === sections.length - 1 && 'opacity-50'}`} 
+									disabled={activeSectionIndex === sections.length - 1}>
+										<ChevronRight className='size-4' /> Next
+									</button>
+
+							</div>
+						</div>
+					</div>
 				</div>
 
 				{/* Right Panel - Preview */}
