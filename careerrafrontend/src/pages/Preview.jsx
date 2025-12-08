cfa0c3966da19bdacbaa82b0c38948f4305e5791
aymanebenomar <aymanebenomar2005@gmail.com>
2025-12-08 22:23:04 +0100

    Implement resume loading functionality in Preview component and add Loader component for loading state

--- careerrafrontend/src/pages/Preview.jsx
+++ careerrafrontend/src/pages/Preview.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyResumeData } from '../assets/assets';
+import Resumepreview from '../components/Resumepreview';
+import { ArrowLeftIcon, Loader } from 'lucide-react';
 
 const Preview = () => {
-  return (
+
+	const {resumeId} = useParams()
+	const [resumeData, setResumeData] = useState();
+	const [isloading, setIsLoading] = useState();
+
+	const loadresume = async() => {
+		setResumeData(dummyResumeData.find(resume => resume._id === resumeId || null))
+		setIsLoading(false)
+	}
+
+	useEffect(() => {
+		loadresume()
+	},[])
+
+  return resumeData ? (
+	<div className='bg-slate-100'>
+		<div className='max-w-3xl mx-auto py-10'>
+			<Resumepreview data={resumeData} template={resumeData.template} accentColor={resumeData.accent_color} 
+			classes='py-4 bg-white'/>
+		</div>
+	</div>
+  ) : (
 	<div>
-		<h1>Preview page</h1>
+		{isloading ? <Loader /> : (
+			<div className='flex flex-col items-center justify-center h-screen'>
+				<p className='text-center text-6xl text-slate-400 font-medium'>Resume not Found</p>
+				<a href='/' className='mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1
+				ring-green-400 flex items-center transition-colors'>
+					<ArrowLeftIcon  className='mr-2 size-4'/> Go to home page
+				</a>
+			</div>
+		)}
 	</div>
   )
 }
