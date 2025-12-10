ab464fd36160c18472fb84083bdd214ef42200fa
aymanebenomar <aymanebenomar2005@gmail.com>
2025-12-10 06:18:50 +0100

    feat: Integrate ImageKit for image uploads in resume creation and set up resume routes

--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -1,10 +1,10 @@
 
-import Resume from "../models/Resume";
+import imageKit from "../configs/imageKit.js";
+import Resume from "../models/Resume.js";
+import fs from 'fs';
 
 // controller for creating a new resume
 // POST: /api/resumes/create
-
-
 export const createResume = async (req, res) => {
 	try {
 
@@ -98,6 +98,23 @@ export const updateResume = async (req, res) => {
 		
 		let resumeDataCopy = JSON.parse(resumeData)
 
+		if(image){
+
+			const imageBufferData = fs.createReadStream(image.path)
+
+			const response = await imageKit.files.upload({
+					file: imageBufferData,
+					fileName: 'resume.png',
+					folder: 'user-resumes',
+					transformation: {
+						pre: 'w-300,h-300,fo-face, z-0.75' + (removeBackground ? ',e-bgremove' : '')
+					}
+
+					});
+
+				resumeDataCopy.personal_info.image = response.url
+		}
+
 		const resume =  await Resume.findOneAndUpdate({userId, _id: resumeId}, resumeDataCopy, {new: true})
 
 		return res.status(200).json({message: 'Saved successfully', resume})
@@ -105,3 +122,4 @@ export const updateResume = async (req, res) => {
 		return res.status(400).json({message: error.message})
 	}
 }
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
 import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(cors())
 
 app.get('/', (req, res) => res.send("Server is Live.. "))
 app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
 
 app.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
